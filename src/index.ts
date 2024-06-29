a53187ce090fb24d94b0ed40ad3efdb8b6f08fe5
BradenBagby <bradenjbagby@gmail.com>
2024-06-29 14:28:27 -0600

    somewhat working

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { join } from 'path';
 import { mkdir as mkDirCallback } from 'fs';
 import { promisify } from 'util';
 import { buildCaption } from './caption-builder';
+import ffmpeg from 'fluent-ffmpeg';
 
 const mkdir = promisify(mkDirCallback);
 
@@ -20,12 +21,60 @@ export const addOverlay = async (
   // build each caption
   const tempDir = join(os.tmpdir(), v4());
   await mkdir(tempDir);
-  const paths: string[] = [];
+  const imageOverlays: ImageOverlay[] = [];
   for (let i = 0; i < overlays.length; i++) {
+    const overlay = overlays[i];
     const path = join(tempDir, `${i}.png`);
-    paths.push(path);
-    await buildCaption(overlays[i].text, bounds, path, config);
+    imageOverlays.push({ ...overlay, path });
+    await buildCaption(overlay.text, bounds, path, config);
   }
 
   // overlay each caption on the video
+  await overlayImagesOnVideo(src, imageOverlays, output);
+};
+
+type ImageOverlay = Overlay & { path: string };
+const overlayImagesOnVideo = async (
+  src: string,
+  overlays: ImageOverlay[],
+  output: string
+) => {
+  const bottomPadding = 16; // TODO:
+
+  // Construct the ffmpeg command
+  const command = ffmpeg();
+  command.input(src);
+
+  // Start building complex filter string
+  const filters: string[] = [];
+  let last = '[0:v]';
+  overlays.forEach((overlay, index) => {
+    command.input(overlay.path);
+    const next = `[outv${index + 1}]`;
+    const x = '(main_w-overlay_w)/2'; // Center x-position
+    const y = `main_h-overlay_h-${bottomPadding}`; // 16 pixels from bottom
+    filters.push(
+      `${last}[${index + 1}:v]overlay=${x}:${y}:enable='between(t,${
+        overlay.start
+      },${overlay.end})'${next}`
+    );
+    last = next;
+  });
+
+  // Apply complex filter
+  const filterString = filters.join(';');
+  await new Promise<void>((resolve, reject) => {
+    command
+      .complexFilter(filterString)
+      .outputOptions('-map', '0:a')
+      // Output
+      .output(output)
+      .outputOptions('-map', `[outv${overlays.length}]`)
+      .on('end', () => resolve())
+      .on('start', command =>
+        console.log(`Spawned Ffmpeg with command: ${command as string}`)
+      )
+      .on('error', err => reject(err))
+      .run();
+  });
 };
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -20,13 +20,12 @@ describe.only('index', () => {
       { start: 7, end: 8, text: 'caption.' },
       { start: 8, end: 9, text: 'It' },
       { start: 9, end: 10, text: 'should' },
-      { start: 10, end: 11, text: 'be' },
-      { start: 11, end: 12, text: 'end.' },
-      { start: 12, end: 13, text: 'at' },
-      { start: 13, end: 14, text: 'exactly' },
-      { start: 14, end: 15, text: '16' },
-      { start: 15, end: 16, text: 'seconds!' },
+      { start: 10, end: 11, text: 'end' },
+      { start: 11, end: 12, text: 'at.' },
+      { start: 12, end: 13, text: 'exactly' },
+      { start: 13, end: 14, text: '15' },
+      { start: 14, end: 15, text: 'seconds!' },
     ];
     await addOverlay(cowVideo, overlays, getTestOutput('cow-captions.mp4'));
-  });
+  }, 10000);
 });
